3982f11260972ac24572b67d9b53a09b7322f5fd
Ryan Rousseau <r@ryanrousseau.com>
2024-03-14 11:01:54 -0500

    Add initial cluster

---
+++ data.tf
@@ -0,0 +1,35 @@
+data "octopusdeploy_environments" "development" {
+  name         = "Development"
+}
+
+data "octopusdeploy_environments" "test" {
+  name         = "Test"
+}
+
+data "octopusdeploy_environments" "production" {
+  name         = "Production"
+}
+
+data "octopusdeploy_projects" "all_octopub" {
+  name         = "All Octopub"
+}
+
+data "octopusdeploy_projects" "mysql" {
+  name         = "MySQL"
+}
+
+data "octopusdeploy_projects" "octopub_audits" {
+  name         = "Octopub Audits"
+}
+
+data "octopusdeploy_projects" "octopub_frontend" {
+  name         = "Octopub Frontend"
+}
+
+data "octopusdeploy_projects" "octopub_products" {
+  name         = "Octopub Products"
+}
+
+data "octopusdeploy_tenants" "awn-dev-12345" {
+  name         = "awn-dev-12345"
+}
